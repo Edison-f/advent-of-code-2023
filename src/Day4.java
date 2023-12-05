688b90360478182cfcda5f7f7adb416bc39dfd87
Edison-f <osgoodbacon@gmail.com>
2023-12-04 22:43:24 -0800

    Make progress on day 4 part 2

--- src/Day4.java
+++ src/Day4.java
@@ -8,9 +8,20 @@ public class Day4 {
         FileInputStream fs = new FileInputStream("./src/day4.txt");
         Scanner in = new Scanner(fs);
         int result = 0;
-        while(in.hasNextLine()) {
-            int pts = 0;
-            String[] split = in.nextLine().split(" \\| ");
+        int[] reruns = new int[204];
+        int[] totals = new int[204];
+        boolean[] seen = new boolean[204];
+        int i = 1;
+        String currString = in.nextLine();
+        int currCard;
+        while(!seen[i] || (i != reruns.length && reruns[i] > 0)) {
+            int matches = 0;
+            seen[i] = true;
+            currCard = Integer.parseInt(currString.split(":")[0].split(" ")[1]);
+            if(i != currCard) {
+                currString = in.nextLine();
+            }
+            String[] split = currString.split(" \\| ");
             String[] winning = split[0].split(": ")[1].split(" ");
             String[] numbers = split[1].split(" ");
             HashSet<Integer> winSet = new HashSet<>();
@@ -23,15 +34,21 @@ public class Day4 {
             for (String s :
                     numbers) {
                 if (!s.isEmpty() && winSet.contains(Integer.parseInt(s))) {
-                    if(pts == 0) {
-                        pts = 1;
-                    } else {
-                        pts = pts << 1;
-                    }
+                    matches++;
+                    reruns[i]--;
                 }
             }
-            result += pts;
-            System.out.println(pts);
+            for(int j = i + 1; j < i + 1 + matches; j++) {
+                reruns[j]++;
+                totals[j]++;
+            }
+            if(reruns[i] == 0) {
+                i++;
+            }
+            System.out.println(matches);
+        }
+        for(int j : totals) {
+            result += j;
         }
         System.out.println(result);
     }
